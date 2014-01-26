683d5dd95c76c7db68227aeb37f73a7a3aa25271
Ryan Conrad <camalot@gmail.com>
2014-01-25 19:16:45 -0600

    Removal of 3 methods that were not meant to be in camalot.common.

--- Camalot.Common/Camalot.Common/Extensions/Type.cs
+++ Camalot.Common/Camalot.Common/Extensions/Type.cs
@@ -29,40 +29,6 @@ namespace Camalot.Common.Extensions {
 		}
 
 		/// <summary>
-		/// Determines whether the specified type an awaitable task.
-		/// </summary>
-		/// <param name="type">The type.</param>
-		/// <returns></returns>
-		public static bool IsAwaitableTask ( this Type type ) {
-			return ( type.IsGenericType && type.GetGenericTypeDefinition ( ).Is<Task> ( ) );
-		}
-
-		/// <summary>
-		/// Gets the type of the underlying.
-		/// </summary>
-		/// <param name="type">The type.</param>
-		/// <returns></returns>
-		public static Type GetUnderlyingType ( this Type type ) {
-			var lowType = type;
-			if ( type.IsAwaitableTask ( ) ) {
-				lowType = type.GenericTypeArguments[0];
-			}
-
-			return lowType.IsGenericType && lowType.GenericTypeArguments.Length == 1 ?
-				lowType.GenericTypeArguments[0] :
-				lowType;
-		}
-
-		/// <summary>
-		/// Determines whether the specified type is a system type.
-		/// </summary>
-		/// <param name="type">The type.</param>
-		/// <returns></returns>
-		public static bool IsSystemType ( this Type type ) {
-			return type.FullName.Equals ( "System" ) || type.FullName.StartsWith ( "System." );
-		}
-
-		/// <summary>
 		/// Determines whether [is] [the specified attribute].
 		/// </summary>
 		/// <typeparam name="TType">The type of the type.</typeparam>
