7d57aa2ad74d653e89d58039742b9354e07ce7e4
Ryan Conrad <camalot@gmail.com>
2014-03-24 21:32:17 -0500

    Updated nuspec for MVC3 to pull in the required mvc3 packages. added msbuild community tasks to build folder. updated install XDT.

---
+++ Camalot.Common/Camalot.Common.UnitTest/InExtensions/ForRequire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+using Camalot.Common.Extensions;
+using Moq;
+namespace Camalot.Common.UnitTest.InExtensions {
+	[TestClass]
+	public class ForRequire {
+
+		public void WhenObjectNotNull_MustNotThrowException ( ) {
+			var fixture = new Fixture ( );
+			var obj = fixture.Build<object>().Create<object>();
+			
+			
+		}
+
+	}
+}
