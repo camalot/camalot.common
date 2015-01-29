3d15b83177cd16b996d102e069c6077a9a047981
Ryan Conrad <camalot@gmail.com>
2015-01-28 20:43:41 -0600

    added TaskHelper to run tasks sync

---
+++ Camalot.Common/Camalot.Common/Extensions/Task.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camalot.Common.Extensions {
+	public static partial class CamalotCommonExtensions {
+		/// <summary>
+		/// Runs the task function synchronously.
+		/// </summary>
+		/// <param name="task">The task.</param>
+		public static void RunSync(this Func<Task> task) {
+			TaskHelper.RunSync(task);
+		}
+		/// <summary>
+		/// Runs the task function synchronously.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="task">The task.</param>
+		/// <returns></returns>
+		public static T RunSync<T>(this Func<Task<T>> task) {
+			return TaskHelper.RunSync<T>(task);
+		}
+
+		/// <summary>
+		/// Runs the task synchronously.
+		/// </summary>
+		/// <param name="task">The task.</param>
+		public static void RunSync(this Task task) {
+			TaskHelper.RunSync(async () => await task);
+		}
+		/// <summary>
+		/// Runs the task synchronously.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="task">The task.</param>
+		/// <returns></returns>
+		public static T RunSync<T>(this Task<T> task) {
+			return TaskHelper.RunSync<T>(async () => await task);
+		}
+	}
+}
